d409ebaf0c244465e63181db283bfe98c97d9a1f
ashot.aydiyan <ashot@aod.am>
2020-02-09 17:46:26 +0400

    done done

--- models/file.js
+++ models/file.js
@@ -10,22 +10,5 @@ module.exports = (sequelize, DataTypes) => {
     size: DataTypes.INTEGER,
     uploaded: DataTypes.DATE
   }, {});
-  File.addHook('beforeSave', async (file) => {
-    const renameDuplicates = async () => {
-      const count = await File.count({
-        where: {
-          name: file.name,
-        }
-      });
-
-      if (count > 0){
-        file.name = `${file.name} (copy)`;
-        await renameDuplicates()
-      }
-    };
-    if(file.isNewRecord){
-      await renameDuplicates()
-    }
-  });
   return File;
 };
--- src/controllers/files.js
+++ src/controllers/files.js
@@ -5,9 +5,9 @@ const { File } = require('../../models');
 
 
 class Files {
-    static upload (req, res) {
+    static async upload (req, res) {
         const { file } = req.files;
-        const filename = file.name;
+        const filename = `${req.user.id}-${new Date().toISOString()}-${file.name}`;
         const fileRecord = {
             path: `./files/${filename}`,
             name: filename,
@@ -16,29 +16,30 @@ class Files {
             size: file.size,
             uploaded: new Date()
         };
-        try{
-            File.create(fileRecord).then((res) => {
-                file.mv(`./files/${res.name}`, err => {
-                    if (err)
-                        return res.status(500).send(err);
+
+        try {
+            const newFile = await File.create(fileRecord);
+            file.mv(`./files/${filename}`, err => {
+                if (err)
+                    return res.status(500).send(err);
+
+                res.json({
+                    success: true,
+                    message: "file uploaded!",
+                    file: newFile
                 })
             });
-            res.json({
-                success: true,
-                message: "file uploaded!",
-            })
         } catch (e) {
             res.json({
                 success: false,
                 message: e.message
             })
         }
-
     }
 
     static async getFiles (req, res) {
-        const { list_size } = req.query || 10;
-        const { page } = req.query || 1;
+        const list_size = req.query.list_size || 10;
+        const page = req.query.page || 1;
 
         const paginate = ({page, list_size}) => {
             const offset = page === 1 ? 0 : (page - 1) * list_size;
@@ -52,7 +53,7 @@ class Files {
 
         try {
             const files = await File.findAll({
-                where :{},
+                where: {},
                 ...paginate({page, list_size})
             });
 
@@ -60,7 +61,7 @@ class Files {
                 success: true,
                 data: files
             })
-        }catch (e) {
+        } catch (e) {
             res.json({
                 success: false,
                 message: e.message
@@ -68,17 +69,17 @@ class Files {
         }
 
     }
-    
-    static async deleteFile (req, res) {
-        const { id } = req.params;
 
-        try{
+    static async deleteFile (req, res) {
+        const {id} = req.params;
+
+        try {
             const file = await File.findOne({
                 where: {id}
             });
 
-            const  deleteFileRecord = (err) => {
-                if(err){
+            const deleteFileRecord = (err) => {
+                if (err) {
                     return res.json({
                         success: false,
                         message: err.message
@@ -86,7 +87,7 @@ class Files {
                 }
 
                 File.destroy({
-                    where: { id }
+                    where: {id}
                 });
                 res.json({
                     success: true,
@@ -95,7 +96,7 @@ class Files {
             };
 
             fs.unlink(file.path, deleteFileRecord);
-        }catch (e) {
+        } catch (e) {
             res.json({
                 success: false,
                 message: e.message
@@ -104,23 +105,23 @@ class Files {
     }
 
     static async getFileById (req, res) {
-        const { id } = req.params;
+        const {id} = req.params;
 
-        try{
+        try {
             const file = await File.findOne({
-                where: { id }
+                where: {id}
             });
 
-            if (file){
+            if (file) {
                 res.json({
                     success: true,
                     file
                 })
-            }else {
+            } else {
                 res.sendStatus(404);
             }
 
-        }catch (e) {
+        } catch (e) {
             res.json({
                 success: false,
                 message: e.message
@@ -129,20 +130,20 @@ class Files {
     }
 
     static async downloadFile (req, res) {
-        const { id } = req.params;
+        const {id} = req.params;
 
-        try{
+        try {
             const file = await File.findOne({
-                where: { id }
+                where: {id}
             });
 
-            if (file){
-               res.download(file.path);
-            }else {
+            if (file) {
+                res.download(file.path);
+            } else {
                 res.sendStatus(404);
             }
 
-        }catch (e) {
+        } catch (e) {
             res.json({
                 success: false,
                 message: e.message
@@ -151,9 +152,9 @@ class Files {
     }
 
     static async updateFile (req, res) {
-        const { file } = req.files;
-        const { id } = req.params;
-        const filename = file.name;
+        const {file} = req.files;
+        const {id} = req.params;
+        const filename = `${req.user.id}-${new Date().toISOString()}-${file.name}`;
         const fileRecord = {
             path: `./files/${filename}`,
             name: filename,
@@ -162,22 +163,46 @@ class Files {
             size: file.size,
             uploaded: new Date()
         };
-        try{
-          const updated = await File.update(fileRecord,{
+
+        const updateFile = async (err) => {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: err.message
+                });
+            }
+            try {
+                const newFile = await File.update(fileRecord, {
+                    where: {id}
+                });
+                file.mv(`./files/${filename}`, err => {
+                    if (err)
+                        return res.status(500).send(err);
+
+                    res.json({
+                        success: false,
+                        message: `${newFile[0]} file updated!`,
+                    });
+                });
+            }catch (e) {
+                return res.json({
+                    success: false,
+                    message: e.message
+
+                });
+            }
+
+        };
+
+        try {
+            const oldFile = await File.findOne({
                 where: { id }
             });
-
-          if(updated[0]){
-              res.json({
-                  success: false,
-                  message: 'file updated!',
-              })
-          }
-          else {
-              res.sendStatus(404);
-          }
-
-        }catch (e) {
+            if(!oldFile){
+                res.sendStatus(404)
+            }
+            fs.unlink(oldFile.path, updateFile);
+        } catch (e) {
             res.json({
                 success: false,
                 message: e.message
