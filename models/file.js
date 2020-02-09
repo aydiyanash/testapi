995d0c7b75874263d40ddc33b5375451d84e35b8
ashot.aydiyan <ashot@aod.am>
2020-02-09 23:58:15 +0400

    add index to file model

--- models/file.js
+++ models/file.js
@@ -4,7 +4,11 @@ import Security from "../src/utils/security";
 module.exports = (sequelize, DataTypes) => {
   const File = sequelize.define('File', {
     path: DataTypes.STRING,
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
     extension: DataTypes.STRING,
     mime: DataTypes.STRING,
     size: DataTypes.INTEGER,
